09c7e4f1e8d3f6468596edfa18f8275eea50f782
Nihil-Yet <rdbel_ozerov@outlook.com>
2025-12-24 01:30:02 +0300

    Deleted move from GraphVisalizerApp.py to main.py, duplicate defenisons deleted. Removed install and run bat scripts

--- GraphViewer/main.py
+++ GraphViewer/main.py
@@ -1,16 +1,5 @@
-from app.GraphFrontend import run_frontend
-from app.ConsoleWidget import get_console
+from GraphVisualizerApp import GraphVisualizerApp
 
 if __name__ == "__main__":
-    try:
-        run_frontend()
-    except KeyboardInterrupt:
-        console = get_console()
-        if console:
-            console.log_system("Приложение завершено по запросу пользователя")
-        print("Приложение завершено.")
-    except Exception as e:
-        console = get_console()
-        if console:
-            console.log_error(f"Критическая ошибка: {e}")
-        raise
+    app = GraphVisualizerApp()
+    app.run()
