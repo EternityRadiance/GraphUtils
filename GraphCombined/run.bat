19f73b5d2855d8f797ccfe412312da4a0d390e8e
Nihil-Yet <rdbel_ozerov@outlook.com>
2025-12-28 00:20:06 +0300

    added messages into run scripts

--- GraphCombined/run.bat
+++ GraphCombined/run.bat
@@ -2,11 +2,15 @@
 cd /d "%~dp0" || exit /b 1
 
 if not exist "venv\" (
+    echo "Create venv..."
     python -m venv venv
     if errorlevel 1 exit /b 1
 )
 
+echo "Activate venv..."
 call venv\Scripts\activate.bat
+echo "Installing requirements..."
 pip install -r requirements.txt >nul 2>&1
+echo "Start..."
 python main.py
 exit /b %errorlevel%
